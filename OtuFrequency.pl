0ea2f2ec5484ad1180cb3dfbb139adc538bb5b35
alexWMH <mumuwu98@gmail.com>
2024-05-22 16:42:38 +0800

    add 2 program

--- OtuFrequency.pl
+++ OtuFrequency.pl
@@ -1,1 +1,62 @@
-/home/alex/bin/OtuFrequency.pl
+#!/usr/bin/perl -w
+# usgae : OtuFrequency.pl otutab.txt
+
+use strict;
+				
+
+# load OTU table
+
+my %is=();
+my %in=();
+
+open(IN,"<$ARGV[0]") || die "open $ARGV[0]: $!\n";
+$_=<IN>; chomp $_;
+my @a=split("\t",$_);
+for(my $i=1;$i<@a;$i++) {
+    $is{$i}=$a[$i];
+}
+while(<IN>) {
+    chomp $_;
+    my @a=split("\t",$_);
+    for(my $i=1;$i<@a;$i++) {
+	$in{$i}+=$a[$i];
+    }
+}
+seek(IN,0,0);
+
+
+# calculate frequency and output (sort by mean frequency)
+
+my %of=();
+my %omf=();
+
+$_=<IN>; print $_;
+while(<IN>) {
+    chomp $_;
+    my @a=split("\t",$_);
+    my $o=$a[0];
+    my @f=();
+    for(my $i=1;$i<@a;$i++) {
+	my $f = $a[$i] == 0 ? 0 : $a[$i]/$in{$i}*100;
+	push(@f,$f);
+    }
+    $of{$o}=join("\t",@f);
+    $omf{$o}=Mean(@f);
+}
+close IN;
+
+foreach my $o (sort{$omf{$b}<=>$omf{$a}}keys %omf) {
+    print "$o\t$of{$o}\n";
+}
+
+
+
+######################################################################
+
+
+sub Mean {
+    my $m=0;
+    foreach my $e (@_) { $m+=$e; }
+    $m/=scalar(@_);
+    return $m;
+}
--- RdpTaxonomyFrequency_species.pl
+++ RdpTaxonomyFrequency_species.pl
@@ -1,1 +1,100 @@
-/home/alex/bin/RdpTaxonomyFrequency_species.pl
+#!/usr/bin/perl -w
+# usage : RdpTaxonomyFrequency.pl otutab.freq otus.rdp
+
+use strict;
+use Getopt::Long;
+
+
+# set paramter
+
+my $cutoff=0.8;
+my $outprefix="method";
+
+GetOptions(
+    "cutoff=f" => \$cutoff,
+    "outprefix=s" => \$outprefix
+    );
+
+
+# load OTU frequency
+
+my %osf=();
+
+open(IN,"<$ARGV[0]") || die "open $ARGV[0]: $!\n";
+$_=<IN>;
+my @sample=split("\t",$_); chomp $sample[-1];
+while(<IN>) {
+    my @a=split("\t",$_); chomp $a[-1];
+    for(my $i=1;$i<@a;$i++) {
+        $osf{$a[0]}{$sample[$i]}=$a[$i];
+    }
+}
+close IN;
+
+shift(@sample);
+
+
+# load taxonomy of OTU representative
+
+my %ltq=();
+my %to=();
+
+open(IN,"<$ARGV[1]") || die "open $ARGV[1]: $!\n";
+while(<IN>) {
+    #$_=~s/"//g; $_=~s/ /_/g;
+    my @a=split("\t",$_); chomp $a[-1];
+    for(my $i=10;$i<@a;$i+=3) {
+        if($a[$i]>=$cutoff) {
+            $ltq{$a[$i-1]}{$a[$i-2]}=1;
+            push(@{$to{"$a[$i-1]_$a[$i-2]"}},$a[0]);
+        }
+    }
+}
+
+my %tsf=();
+
+foreach my $t (keys %to) {
+    foreach my $s (@sample) {
+	foreach my $o (@{$to{$t}}) {
+	    if(!$osf{$o}{$s}) { $osf{$o}{$s}=0; }
+	}
+		#my ($sp)=$t=~/.+\_(.+)/;
+        my @a=map($osf{$_}{$s},@{$to{$t}});
+        $tsf{$t}{$s}=Sum(@a);
+    }
+}
+
+# calculate taxonomy frequency and output
+
+my @level=("phylum","class","order","family","genus","species");
+
+foreach my $l (@level) {
+    open(OUT,">$outprefix.$l.freq") || die "open $outprefix.$l.freq: $!\n";
+    print OUT "taxonomy\t".join("\t",@sample)."\n";
+    my @tax=keys %{$ltq{$l}};
+    my %tasf=();
+    my %ttsf=();
+    foreach my $t (@tax) {
+	$tasf{$t}=$t;
+        foreach my $s (@sample) {
+	    $tasf{$t}.="\t".sprintf("%.4f",$tsf{"$l\_$t"}{$s});     # $tasf{$t}.="\t".sprintf("%.3f",$tsf{"$l\_$t"}{$s});
+	    $ttsf{$t}+=$tsf{"$l\_$t"}{$s};
+        }
+    }
+    @tax=sort{$ttsf{$b}<=>$ttsf{$a}}@tax;
+    foreach my $t (@tax) {
+	print OUT $tasf{$t}."\n";
+    }
+    close OUT;
+}
+
+
+
+######################################################################
+
+
+sub Sum {
+    my $s=0;
+	foreach my $e (@_) { if($e){$s+=$e};}
+    return $s;
+}
